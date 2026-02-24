6a0e4839ef5489d9d4c843672dca9405e88cc21b
btrintm-beep <btrintm@gmail.com>
2026-02-24 16:49:00 -0700

    Remove histogram plot and refine comments
    Removed matplotlib plotting and updated comments for clarity.

--- Question1.py
+++ Question1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 
 df = pd.read_csv('crime1 (1).csv')
@@ -19,11 +19,10 @@ print("Std Value: ", std_value)
 print("Min Value: ", min_value)
 print("Max Value: ", max_value)
 
-plt.hist(column)
-plt.show()
+
 
 #The mean value is larger than the median value and we can
-#see that the graph is right skewed
+#assume that the graph is right skewed
 
 #outliers effect the mean more than the median as the mean uses an average of every number which the outlier would skew
 #while the median goes through the middle value which even with an addition of outliers should not change it to much since its so
